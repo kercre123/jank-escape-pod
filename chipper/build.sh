fd175d76eb62d419ff655cdc70bad937be7e414e
Kerigan <whyyoulittle333@gmail.com>
2022-05-27 20:29:45 -0500

    Use golang binary package binary

--- chipper/build.sh
+++ chipper/build.sh
@@ -5,7 +5,7 @@ if [[ $(arch) == "aarch64" ]]; then
 elif [[ $(arch) == "x86_64" ]]; then
    ARCH=amd64
 fi
-CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} go build \
+CGO_ENABLED=0 GOOS=linux GOARCH=${ARCH} /usr/local/go/bin/go build \
 -ldflags "-w -s -extldflags "-static"" \
 -trimpath \
 -o chipper cmd/main.go
