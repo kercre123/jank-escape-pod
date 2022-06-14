a566d9d5d4bdb31daea0ee4957d6d67e713012f3
Wire <whyyoulittle333@gmail.com>
2022-06-13 22:30:41 -0500

    Support for multiple bots at a time

--- chipper/pkg/voice_processors/noop/intent.go
+++ chipper/pkg/voice_processors/noop/intent.go
@@ -1,14 +1,16 @@
 package noop
 
 import (
-	pb "github.com/digital-dream-labs/api/go/chipperpb"
-	"github.com/digital-dream-labs/chipper/pkg/vtt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
-	"io/ioutil"
+
+	pb "github.com/digital-dream-labs/api/go/chipperpb"
+	"github.com/digital-dream-labs/chipper/pkg/vtt"
 )
 
 func check(e error) {
@@ -20,6 +22,7 @@ func check(e error) {
 var intent pb.IntentResponse
 var matched int = 0
 var intentNum int = 0
+var botNum int = 0
 
 func IntentPass(req *vtt.IntentRequest, intentThing string) (*vtt.IntentResponse, error) {
 	intent = pb.IntentResponse{
@@ -60,9 +63,13 @@ func (s *Server) ProcessIntent(req *vtt.IntentRequest) (*vtt.IntentResponse, err
 	var finished4 string
 	var transcribedText string
 	matched = 0
-	f, err := os.Create("/tmp/voice.ogg")
+	botNum = botNum + 1
+	log.Println(botNum)
+	log.Println(strconv.Itoa(botNum))
+	log.Println(strconv.Itoa(botNum))
+	f, err := os.Create("/tmp/" + strconv.Itoa(botNum) + "voice.ogg")
 	check(err)
-	cmd1 := exec.Command("/bin/bash", "../stt.sh")
+	cmd1 := exec.Command("/bin/bash", "../stt.sh", strconv.Itoa(botNum))
 	data := []byte{}
 	data = append(data, req.FirstReq.InputAudio...)
 	cmd1.Run()
@@ -78,52 +85,53 @@ func (s *Server) ProcessIntent(req *vtt.IntentRequest) (*vtt.IntentResponse, err
 		}
 		data = append(data, chunk.InputAudio...)
 		f.Write(chunk.InputAudio)
-		fileBytes1, _ := ioutil.ReadFile("/tmp/utterance1")
+		fileBytes1, _ := ioutil.ReadFile("/tmp/" + strconv.Itoa(botNum) + "utterance1")
 		transcribedText1 := strings.TrimSpace(string(fileBytes1))
-		if _, err := os.Stat("/tmp/utterance1"); err == nil {
+		if _, err := os.Stat("/tmp/" + strconv.Itoa(botNum) + "utterance1"); err == nil {
 			finished1 = transcribedText1
 		}
 		if _, err := os.Stat("./armarch"); err == nil {
-			if _, err := os.Stat("/tmp/sttDone"); err == nil {
+			if _, err := os.Stat("/tmp/" + strconv.Itoa(botNum) + "sttDone"); err == nil {
 				transcribedText = finished1
 				log.Println("aarch: Speech has stopped, transcribed text is: " + finished1)
 				break
 			}
 		} else {
-			fileBytes2, _ := ioutil.ReadFile("/tmp/utterance2")
+			fileBytes2, _ := ioutil.ReadFile("/tmp/" + strconv.Itoa(botNum) + "utterance2")
 			transcribedText2 := strings.TrimSpace(string(fileBytes2))
-			if _, err := os.Stat("/tmp/utterance2"); err == nil {
+			if _, err := os.Stat("/tmp/" + strconv.Itoa(botNum) + "utterance2"); err == nil {
 				finished2 = transcribedText2
-				if finished1 == finished2  {
+				if finished1 == finished2 {
 					transcribedText = finished2
 					log.Println("2: Speech has stopped, transcribed text is: " + finished2)
 					break
 				}
 			}
-			fileBytes3, _ := ioutil.ReadFile("/tmp/utterance3")
+			fileBytes3, _ := ioutil.ReadFile("/tmp/" + strconv.Itoa(botNum) + "utterance3")
 			transcribedText3 := strings.TrimSpace(string(fileBytes3))
-			if _, err := os.Stat("/tmp/utterance3"); err == nil {
+			if _, err := os.Stat("/tmp/" + strconv.Itoa(botNum) + "utterance3"); err == nil {
 				finished3 = transcribedText3
-				if finished2 == finished3  {
+				if finished2 == finished3 {
 					transcribedText = finished3
 					log.Println("3: Speech has stopped, transcribed text is: " + finished3)
 					break
 				}
 			}
-			fileBytes4, _ := ioutil.ReadFile("/tmp/utterance4")
+			fileBytes4, _ := ioutil.ReadFile("/tmp/" + strconv.Itoa(botNum) + "utterance4")
 			transcribedText4 := strings.TrimSpace(string(fileBytes4))
-			if _, err := os.Stat("/tmp/utterance4"); err == nil {
+			if _, err := os.Stat("/tmp/" + strconv.Itoa(botNum) + "utterance4"); err == nil {
 				finished4 = transcribedText4
-				if finished3 == finished4  {
+				if finished3 == finished4 {
 					transcribedText = finished4
 					log.Println("4: Speech has stopped, transcribed text is: " + finished4)
 					break
 				}
 			}
-		} 
+		}
 	}
 
 	processTextAll(req, transcribedText, matchListList, intentsList)
+	botNum = botNum - 1
 
 	if matched == 0 {
 		log.Println("No intent was matched.")
@@ -131,4 +139,3 @@ func (s *Server) ProcessIntent(req *vtt.IntentRequest) (*vtt.IntentResponse, err
 	}
 	return nil, nil
 }
-
--- stt.sh
+++ stt.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
 
+botNum=$1
+
 function sstCmd() {
-STT_TFLITE_DELEGATE=gpu ./stt/stt --model ./stt/model.tflite --scorer ./stt/large_vocabulary.scorer --audio /tmp/voice.wav
+STT_TFLITE_DELEGATE=gpu ./stt/stt --model ./stt/model.tflite --scorer ./stt/large_vocabulary.scorer --audio /tmp/${botNum}voice.wav
 }
 
 function ffmpegCmd() {
-ffmpeg -y -i /tmp/voice.ogg /tmp/voice.wav
+ffmpeg -y -i /tmp/${botNum}voice.ogg /tmp/${botNum}voice.wav
 }
 
 function doSttARM() {
 sleep 0.8
 cd ../
-rm -r /tmp/voice.wav
+rm -r /tmp/*voice.wav
 ffmpegCmd
-sstCmd > /tmp/utterance1
+sstCmd > /tmp/${botNum}utterance1
 touch /tmp/sttDone
 sleep 0.5
-rm -f /tmp/utterance*
-rm -f /tmp/voice.wav
-rm -f /tmp/sttDone
+rm -f /tmp/*utterance*
+rm -f /tmp/*voice.wav
+rm -f /tmp/*sttDone
 }
 
 function doSttAMD64() {
 sleep 0.8
 cd ../
-rm -r /tmp/voice.wav
+rm -r /tmp/*voice.wav
 ffmpegCmd
-sstCmd > /tmp/utterance1
+sstCmd > /tmp/${botNum}utterance1
 sleep 0.5
 ffmpegCmd
-sstCmd > /tmp/utterance2
+sstCmd > /tmp/${botNum}utterance2
 ffmpegCmd
-sstCmd > /tmp/utterance3
+sstCmd > /tmp/${botNum}utterance3
 ffmpegCmd
-sstCmd > /tmp/utterance4
+sstCmd > /tmp/${botNum}utterance4
 sleep 0.5
-rm -rf /tmp/utterance*
-rm -rf /tmp/voice.wav
+rm -rf /tmp/*utterance*
+rm -rf /tmp/*voice.wav
 }
 
 UNAME=$(uname -a)
