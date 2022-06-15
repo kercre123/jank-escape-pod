92f263037370b509a8ae2185dc5c2e97827b0734
Wire <whyyoulittle333@gmail.com>
2022-06-15 13:51:12 -0500

    Case for if 3rd and 4th utterances dont match, fix support for multiple bots

--- stt.sh
+++ stt.sh
@@ -17,20 +17,20 @@ ffmpeg -y -i /tmp/${botNum}voice.ogg /tmp/${botNum}voice.wav
 function doSttSlow() {
 sleep 2
 cd ../
-rm -r /tmp/*voice.wav
+rm -rf /tmp/${botNum}voice.wav
 ffmpegCmd
 sstCmd > /tmp/${botNum}utterance1
 touch /tmp/${botNum}sttDone
 sleep 0.5
-rm -f /tmp/*utterance*
-rm -f /tmp/*voice.wav
-rm -f /tmp/*sttDone
+rm -f /tmp/${botNum}utterance*
+rm -f /tmp/${botNum}voice.wav
+rm -f /tmp/${botNum}sttDone
 }
 
 function doSttFast() {
 sleep 0.8
 cd ../
-rm -r /tmp/*voice.wav
+rm -rf /tmp/${botNum}voice.wav
 ffmpegCmd
 sstCmd > /tmp/${botNum}utterance1
 sleep 0.5
@@ -40,12 +40,13 @@ ffmpegCmd
 sstCmd > /tmp/${botNum}utterance3
 ffmpegCmd
 sstCmd > /tmp/${botNum}utterance4
+touch /tmp/${botNum}sttDone
 sleep 0.5
-rm -rf /tmp/*utterance*
-rm -rf /tmp/*voice.wav
+rm -rf /tmp/${botNum}utterance*
+rm -rf /tmp/${botNum}voice.wav
+rm -rf /tmp/${botNum}sttDone
 }
 
-UNAME=$(uname -a)
 if [[ -f slowsys ]]; then
   doSttSlow &
 else
