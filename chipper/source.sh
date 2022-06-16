3cde3ecf52870c684903f9e2c877b5848e7d8eae
Wire <whyyoulittle333@gmail.com>
2022-06-15 19:04:27 -0500

    Remove source.sh from git

--- chipper/source.sh
+++
@@ -1,8 +0,0 @@
-export DDL_RPC_PORT=443
-export DDL_RPC_TLS_CERTIFICATE=$(cat ../certs/cert.crt)
-export DDL_RPC_TLS_KEY=$(cat ../certs/cert.key)
-export DDL_RPC_CLIENT_AUTHENTICATION=NoClientCert
-export WEATHERAPI_ENABLED=true
-export WEATHERAPI_KEY=580a7d8a1837426b9af220858221506
-export WEATHERAPI_UNIT=F
-export DEBUG_LOGGING=true
